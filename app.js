9b5ae48d7017051a677610b491ff55e78ac1f9cc
William H. Rivera <bill_dev_data@outlook.com>
2025-03-17 07:45:27 -0500

    Restringiendo la función de agregar nombres para que sólo funciones si es que hay un nombre escrito

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ let listaSorteo = "";
 
 function agregarAmigo(){
     let nuevoAmigo = document.getElementById('amigo').value;
-    let listaAmigos = document.getElementById('listaAmigos'); //Asignando variable a elemento UL
-    listaSorteo.push(nuevoAmigo); //Agregando nuevo amigo al array del sorteo
-    listaAmigos.innerHTML = ""; //Vaciar lista anterior (elemento UL) para crear nueva lista
-    for (let i=0; i<listaSorteo.length;i++){
-        let nombre = listaSorteo[i]; //Loop extrae cada nombre (texto)
-        let listItem = document.createElement("LI"); //Creando elemento LI
-        listItem.textContent = nombre; // Se le asigna el texto del nombre al elemento LI
-        listaAmigos.appendChild(listItem); //Se le adiciona el elemento LI a UL. Se construye nueva lista
+    if (nuevoAmigo != ""){
+        let listaAmigos = document.getElementById('listaAmigos'); //Asignando variable a elemento UL
+        listaSorteo.push(nuevoAmigo); //Agregando nuevo amigo al array del sorteo
+        listaAmigos.innerHTML = ""; //Vaciar lista anterior (elemento UL) para crear nueva lista
+        for (let i=0; i<listaSorteo.length;i++){
+            let nombre = listaSorteo[i]; //Loop extrae cada nombre (texto)
+            let listItem = document.createElement("LI"); //Creando elemento LI
+            listItem.textContent = nombre; // Se le asigna el texto del nombre al elemento LI
+            listaAmigos.appendChild(listItem); //Se le adiciona el elemento LI a UL. Se construye nueva lista
+        }
+        limpiarCaja(); //Limpiar caja luego de agregar nombre.
+    } else {
+        alert("Por favor, inserte un nombre"); //En caso de campo vacío
     }
-    limpiarCaja(); //Limpiar caja luego de agregar nombre.
 }
 
 function limpiarCaja(){
