104fea287022f753faad6cc85b100a48a3a74b76
William H. Rivera <bill_dev_data@outlook.com>
2025-03-17 08:10:24 -0500

    Se creo la función para sortear. Se hizo global la variable listaAmigos para poder ser referenciada en ambas funciones

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaSorteo = "";
+let listaAmigos = document.getElementById('listaAmigos'); //Asignando variable a elemento UL
 
 function agregarAmigo(){
     let nuevoAmigo = document.getElementById('amigo').value;
     if (nuevoAmigo != ""){
-        let listaAmigos = document.getElementById('listaAmigos'); //Asignando variable a elemento UL
         listaSorteo.push(nuevoAmigo); //Agregando nuevo amigo al array del sorteo
         listaAmigos.innerHTML = ""; //Vaciar lista anterior (elemento UL) para crear nueva lista
         for (let i=0; i<listaSorteo.length;i++){
@@ -19,6 +19,21 @@ function agregarAmigo(){
         alert("Por favor, inserte un nombre"); //En caso de campo vacío
     }
 }
+
+
+function sortearAmigo(){
+    let numElementos = listaSorteo.length;
+    let resultado = document.getElementById('resultado'); //Se declara variable resultado, asociado al elemento UL del mismo nombre
+    if (numElementos > 1){ //La función solo trabaja si es que hay 2 o más nombres registrados
+        let numSorteo = parseInt(Math.floor(Math.random()*numElementos));
+        listaAmigos.innerHTML = ""; //Vaciando lista para nuevo sorteo
+        resultado.innerHTML = `Tu amigo secreto es: ${listaSorteo[numSorteo]}`; //Sortea el indice del array y muestra el resultado
+        estadoInicial();
+    } else {
+        alert("Debes ingresar los nombres de más amigos");
+    }
+}
+
 
 function limpiarCaja(){
     document.getElementById('amigo').value = ""
